e9fc70145b377b71cc73659a96d2f875bff9aa73
rehema_funani <rehemafunani@gmail.com>
2024-03-07 08:18:48 +0300

    first commit

---
+++ strings.js
@@ -0,0 +1,32 @@
+let a = "extravaganza";
+console.log(a[8]+""+a[9] +""+a[10]+""+a[11]);
+
+let b = "wonderful"
+console.log(b.toUpperCase());
+
+let c = "amazing";
+console.log(c.toUpperCase());
+
+let d = "undERneath";
+console.log(d.toUpperCase());
+
+const string1 = "The pupils are reading in the library";
+console.log(string1[11]+""+string1[12]+""+string1[13]);
+
+const string2 = "The child was sitting on the table before it fell"
+console.log(string2[14]+""+string2[15]+""+string2[16]+""+string2[17]+""+string2[18]+""+string2[19]+""+string2[20]);
+
+const story = "The quick brown fox jumps over the lazy dog";
+console.log((story.match(/the/)).length);
+
+const story2 = "The quick brown fox jumps over the lazy dog";
+console.log((story.match(/brown/)).length);
+
+const food = "The quick fox jumped over the lazy dog";
+console.log(food.replace("q","ate"));
+
+
+
+let e = "A wonderful world";
+console.log(e.replace("w","W") );
+
